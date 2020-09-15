456dab874f46190554c1ae0803505b8250b25286
DallasG19 <70603629+DallasG19@users.noreply.github.com>
2020-09-15 11:22:32 -0400

    Add files via upload

---
+++ HeadFirst/src/main/java/Main.java
@@ -0,0 +1,40 @@
+
+public class Main {
+    public static void main(String[] args) {
+       /*
+(1.)Beer Song repeated the first line twice.
+
+        int beerNum = 99;
+        String word = "bottles";
+        while(beerNum>0){
+            if (beerNum==1) {
+                word = "bottle";
+            }
+            beerNum = beerNum-1;
+            if (beerNum>0){
+                System.out.println(beerNum + " " + word + " of beer on the wall");
+                System.out.println(beerNum + " " + word + " of beer");
+                System.out.println("Take one down");
+                System.out.println("Pass it around");
+            }else{
+                System.out.println("No more bottles of beer on the wall");
+            }
+        } */
+///////////////////////////////////////////////////////////////
+/*
+(2. array of words)
+        String[] wordlist1 = {"Pizza", "Popcorn", "Steak", "Cheese Burger"};
+        String[] wordlist2 = {"Java", "JavaScript", "Html", "CSS", };
+        String[] wordlist3 = {"Words", "Things", "Something", "HW", };
+        int onelength = wordlist1.length;
+        int twolength = wordlist2.length;
+        int threelength = wordlist3.length;
+        int rand1 = (int) (Math.random() + onelength);
+        int rand2 = (int) (Math.random() + twolength);
+        int rand3 = (int) (Math.random() + threelength);
+        String phrase = wordlist1[rand1-1] + " " + wordlist2[rand2-1]  +" "+ wordlist3[rand3-1];
+        System.out.println("What we need is a " + phrase);
+ */
+/////////////////////////////////////////////////////////////////////////
+    }
+}
